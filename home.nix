0493c5badf435f4fd0a66e93b27f6903b14547cd
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-04-03 15:41:09 -0700

    fzf module

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./home/shell/zsh.nix
+    ./home/shell/fzf.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -61,21 +62,5 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.fzf = {
-    enable = true;
-    colors = {
-      "bg+" = "#313244";
-      "bg" = "#1e1e2e";
-      "spinner" = "#f5e0dc";
-      "hl" = "#f38ba8";
-      "fg" = "#cdd6f4";
-      "header" = "#f38ba8";
-      "info" = "#cba6f7";
-      "pointer" = "#f5e0dc";
-      "marker" = "#f5e0dc";
-      "fg+" = "#cdd6f4";
-      "prompt" = "#cba6f7";
-      "hl+" = "#f38ba8";
-      };
-  };
+    };
 }
