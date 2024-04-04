f17df874807318c5c042339c41dd47ad11acf364
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-04-03 19:44:38 -0700

    Base config

---
+++ configuration.nix
@@ -0,0 +1,44 @@
+# Configuration.nix
+
+{ config, pkgs, ... }:
+{
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  console.font = "Lat2-Terminus16"
+  programs.zsh.enable = true;
+
+  networking.networkmanager.enable = true;
+
+  # Timezone and localization
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    bind #nslookup
+    git
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.allowed-users = [ "@wheel" ];
+  
+
+ 
+}
+
