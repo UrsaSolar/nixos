d0714541669bc5ed38e4279dc07d04d4863134e8
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-04-05 09:43:04 -0700

    Rename

--- home/apps/firefox.nix
+++ home/apps/firefox.nix
@@ -7,7 +7,7 @@
       id = 0;
       name = "solarbear";
       isDefault = true;
-      userChrome = builtins.fileRead (./userChrome.css);
+      userChrome = builtins.fileRead (./firefox-userChrome.css);
       search = {
         default = "DuckDuckGo";
         force = true;
