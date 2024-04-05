394ad227a64f7b70de04e115dc771623a436ac9e
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-04-05 09:23:08 -0700

    Neotree

--- home/shell/neovim.nix
+++ home/shell/neovim.nix
@@ -6,13 +6,19 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    extraConfig = ''colorscheme catppuccin-mocha'';
+    extraConfig = ''
+      colorscheme catppuccin-mocha
+      nmap <F1> :Neotree toggle=true<CR>
+    '';
     plugins = with pkgs.vimPlugins; [
-      catppuccin-nvim
+      { plugin = neo-tree-nvim;
+          type = "lua";
+          config = builtins.readFile(./neotree-config.lua); }
       { plugin = lualine-nvim;
-        type = "lua";
-	config = builtins.readFile(./lualine-config.lua);
-      }
+          type = "lua";
+	  config = builtins.readFile(./lualine-config.lua); }
+    catppuccin-nvim
+    which-key-nvim
     ];
   };
 
