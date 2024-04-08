18107630ea3622c6445ef6ee4a5bc3154786e7d4
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-04-08 11:26:17 -0700

    Add bluetooth support

--- hosts/luna.nix
+++ hosts/luna.nix
@@ -36,6 +36,15 @@ in
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  hardware.bluetooth = {
+    enable = true; # enables support for Bluetooth
+    powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    settings.general = {
+      enable = "Source,Sink,Media,Socket"; # Enable A2DP sink
+      experimental = true; # Enables battery reporting
+    };
+  };
+
   # Nvidia
   hardware.opengl = {
     enable = true;
