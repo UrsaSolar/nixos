7ac937eb11c9073e1fcdbf015f9b5665b90b2d6f
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-04-11 12:50:28 -0700

    Add btop

---
+++ home/terminal/btop.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:{
+
+  programs.btop = {
+    enable = true;
+    catppuccin.enable = true;
+    settings = {
+      theme_background = false;
+      truecolor = true;
+      rounded_corners = true;
+      update_ms = 1000;
+      base_10_sizes = true;
+      cpu_single_graph = true;
+      # mem_graphs = false;
+      io_mode = true; # Disk IO graph
+      swap_disk = false; # Show swap as memory
+      proc_sorting = "cpu direct"; # instantly update process list sorting
+      proc_gradient = false;
+      proc_per_core = true; # 100% = one core
+      proc_filter_kernel = true; # filter out kernel processes
+    };
+  };
+
+}
