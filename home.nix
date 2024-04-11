77ab6037acab2a0166c246aada8362e94828438d
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-04-11 09:50:20 -0700

    Fix session variables

--- home.nix
+++ home.nix
@@ -34,19 +34,19 @@
 #    XDG_STATE_HOME="$HOME/.local/state";
 #    XDG_CACHE_HOME="$HOME/.cache";
     # Dotfile cleanliness
-    HISTFILE="$XDG_STATE_HOME/bash/history";
-    LESSHISTFILE="$XDG_STATE_HOME/less/history";
-    INPUTRC="$XDG_CONFIG_HOME/readline/inputrc";
-    ANDROID_HOME="$XDG_DATA_HOME/android";
-    CUDA_CACHE_PATH="XDG_CACHE_HOME/nv";
-    GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc";
-    WGETRC="$XDG_CONFIG_HOME/wgetrc";
-    WINEPREFIX="$XDG_DATA_HOME/wineprefixes/default";
-    XINITRC="$XDG_CONFIG_HOME/X11/xinitrc";
-    XSERVERRC="$XDG_CONFIG_HOME/X11/xserverrc";
-    XCURSOR_PATH="/usr/share/icons:$XDG_DATA_HOME/icons";
-    KDEHOME="$XDG_CONFIG_HOME/kde";
-    NIXOSDIR="$XDG_CONFIG_HOME/nixos";
+    HISTFILE="${config.home.sessionVariables.XDG_STATE_HOME}/bash/history";
+    LESSHISTFILE="${config.home.sessionVariables.XDG_STATE_HOME}/less/history";
+    INPUTRC="${config.home.sessionVariables.XDG_CONFIG_HOME}/readline/inputrc";
+    ANDROID_HOME="${config.home.sessionVariables.XDG_DATA_HOME}/android";
+    CUDA_CACHE_PATH="${config.home.sessionVariables.XDG_CACHE_HOME}/nv";
+    GTK2_RC_FILES="${config.home.sessionVariables.XDG_CONFIG_HOME}/gtk-2.0/gtkrc";
+    WGETRC="${config.home.sessionVariables.XDG_CONFIG_HOME}/wgetrc";
+    WINEPREFIX="${config.home.sessionVariables.XDG_DATA_HOME}/wineprefixes/default";
+    XINITRC="${config.home.sessionVariables.XDG_CONFIG_HOME}/X11/xinitrc";
+    XSERVERRC="${config.home.sessionVariables.XDG_CONFIG_HOME}/X11/xserverrc";
+    XCURSOR_PATH="/usr/share/icons:${config.home.sessionVariables.XDG_DATA_HOME}/icons";
+    KDEHOME="${config.home.sessionVariables.XDG_CONFIG_HOME}/kde";
+    NIXOSDIR="${config.home.sessionVariables.XDG_CONFIG_HOME}/nixos";
   };
 
   
