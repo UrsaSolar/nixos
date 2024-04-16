3b428abea533cbc66c7a8445a959fd79e35bd29c
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-04-16 10:09:50 -0700

    Add DNS swap alias

--- home/kenglish-home.nix
+++ home/kenglish-home.nix
@@ -3,5 +3,12 @@
 {
   home.username = "kenglish";
   home.homeDirectory = "/home/kenglish";
+
+  home.shellAliases = {
+    
+    # Shell shortcuts
+    nm-asm = "nmcli c up \"Allegiant Stadium\"";
+    nm-sol = "nmcli c up \"Sol\"";
+  };
 }
 
