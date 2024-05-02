5fbd6c3f3014a12a5dbb36b7df9788416c33104c
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-05-02 12:58:38 -0700

    Restructure packages for easier management

--- system/graphical.nix
+++ system/graphical.nix
@@ -1,22 +1,6 @@
 { config, pkgs, unstable, ... }:
 {
-
-  # Plasma Desktop
-  services.xserver.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.desktopManager.plasma5.bigscreen.enable = true;
-  programs.dconf.enable = true; # Fix for Firefox not seeing mouse cursor settings; https://github.com/NixOS/nixpkgs/issues/207339#issuecomment-1747101887
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.autoNumlock = true;
-  #services.xserver.displayManager.sddm.theme = "elegant-sddm";
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
+  
   # Pipewire with PulseAudio and Alsa support
   security.rtkit.enable = true;
   services.pipewire = {
@@ -28,31 +12,18 @@
   };
 
   services.flatpak.enable = true;
-
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
   environment.systemPackages = with pkgs; [
-    sddm-kcm # SDDM config module
-    xdg-desktop-portal
-    xdg-desktop-portal-kde
-    plasma-browser-integration
-    catppuccin-papirus-folders
-    breeze-gtk
-    breeze-icons
     clinfo
     glxinfo
     vulkan-tools
     wayland-utils
-    kate
     firefox
     thunderbird
     ungoogled-chromium
     vlc
     iosevka
-
   ];
-
-
 
 }
---
+++ system/plasma.nix
@@ -0,0 +1,33 @@
+{ pkgs, config, ... }: {
+
+ # Plasma Desktop
+  services.xserver.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma5.bigscreen.enable = true;
+  programs.dconf.enable = true; # Fix for Firefox not seeing mouse cursor settings; https://github.com/NixOS/nixpkgs/issues/207339#issuecomment-1747101887
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.autoNumlock = true;
+  #services.xserver.displayManager.sddm.theme = "elegant-sddm";
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+ 
+  environment.systemPackages = with pkgs; [
+    sddm-kcm # SDDM config module
+    kate
+    kwrite
+    xdg-desktop-portal
+    xdg-desktop-portal-kde
+    plasma-browser-integration
+    catppuccin-papirus-folders
+    breeze-gtk
+    breeze-icons
+    catppuccin-kvantum
+    strawberry
+    kcalc
+  ];
+}
