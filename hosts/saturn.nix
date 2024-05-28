02ef8ba58b7046eece19d7b7f031bbd86025df9e
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-05-28 09:48:00 -0700

    Add key

--- hosts/saturn.nix
+++ hosts/saturn.nix
@@ -18,6 +18,7 @@
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUHyx/4I3LxcmzRp9d1+MLd4lt0RyctsiqyfOnBXSXl solarbear@terra"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxtCabfWGBSoEY4spPRPJLAAT6dM22ElBdnoxCiPDlU kenglish@nixos-asm"
     ];
   };
 
--- system/ssh-authorizedkeys.nix
+++ system/ssh-authorizedkeys.nix
@@ -2,5 +2,6 @@
 {
   users.users.solarbear.openssh.authorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUHyx/4I3LxcmzRp9d1+MLd4lt0RyctsiqyfOnBXSXl solarbear@terra"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxtCabfWGBSoEY4spPRPJLAAT6dM22ElBdnoxCiPDlU kenglish@nixos-asm"
   ];
 }
