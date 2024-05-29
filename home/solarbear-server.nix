0c21ca5d38b336ad1d799d481b865a214b1f6b47
Solar Bear <1395386+UrsaSolar@users.noreply.github.com>
2024-05-29 14:22:35 -0700

    Separate server and workstation homes

---
+++ home/solarbear-server.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, unstable, ... }:
+
+{
+  
+  imports = [
+#    ./graphical.nix
+#    ./apps/gaming.nix
+#    ./apps/emulation.nix
+  ];
+
+  home.username = "solarbear";
+  home.homeDirectory = "/home/solarbear";
+
+}
