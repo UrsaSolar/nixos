58afdf3c6378d6acff06d7c3ec4ebe8afde5687b
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-06-04 16:58:34 -0700

    Emulation

--- system/emulation.nix
+++ system/emulation.nix
@@ -1,33 +1,37 @@
 { lib, config, pkgs, unstable, ... } :
 {
 
-   environment.systemPackages = with pkgs; [
-    retroarch
+  environment.systemPackages = with pkgs; [
+    #rpcs3
     retroarch-assets          #unsure if needed, test later
     libretro-core-info        # unsure if needed, test later
-    retroarch-joypad-assets   # unsure if needed, test later
-    libretro.nestopia
-    libretro.bsnes
-    libretro.bsnes-hd
-    libretro.mupen64plus # unsure if both needed
-    libretro.parallel-n64 # unsure if both needed
-    libretro.sameboy
-    libretro.mgba
-    libretro.desmume
-    libretro.melonds
-    libretro.genesis-plus-gx
-    libretro.blastem
-    libretro.picodrive
-    libretro.beetle-saturn
-    libretro.flycast
-    libretro.beetle-psx
-    libretro.beetle-psx-hw
-    libretro.pcsx2
-    libretro.play
-    libretro.ppsspp
-    libretro.fbneo
-    #rpcs3
-
+    retroarch-joypad-autoconfig   # unsure if needed, test later
+    ( retroarch.override
+      {
+        cores = with libretro; [
+          nestopia
+          bsnes
+          bsnes-hd
+          mupen64plus # unsure if both needed
+          parallel-n64 # unsure if both needed
+          sameboy
+          mgba
+          desmume
+          melonds
+          genesis-plus-gx
+          blastem
+          picodrive
+          beetle-saturn
+          flycast
+          beetle-psx
+          beetle-psx-hw
+          pcsx2
+          play
+          ppsspp
+          fbneo
+        ];
+      }
+    )
   ];
 
 }
