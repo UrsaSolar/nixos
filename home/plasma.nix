d008c10e9fd34b769faf9f4b7610304021332a1a
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-06-04 18:59:33 -0700

    Remove plasma-manager for now

--- home/plasma.nix
+++ home/plasma.nix
@@ -4,18 +4,18 @@
   programs.plasma = {
     enable = true;
     configFile.kcminputrc.Keyboard.NumLock.value = 0; # Enable numlock on boot
-    workspace ={
-      clickItemTo = "select";
-      lookAndFeel = "org.kde.breezedark.desktop";
-      cursorTheme = "Default";
-      iconTheme = "Papirus-Dark";
-      colorScheme = "CatppuccinMochaFlamingo";
-    };
-    hotkeys.commands."launch-konsole" = {
-      name = "Launch Konsole";
-      key = "Meta+Alt+K";
-      command = "konsole";
-    };
+    #workspace = {
+    #  clickItemTo = "select";
+    #  lookAndFeel = "org.kde.breezedark.desktop";
+    #  cursorTheme = "Default";
+    #  iconTheme = "Papirus-Dark";
+    #  colorScheme = "CatppuccinMochaFlamingo";
+    #};
+    #hotkeys.commands."launch-konsole" = {
+    #  name = "Launch Konsole";
+    #  key = "Meta+Alt+K";
+    #  command = "konsole";
+    #};
   };
 
   services.kdeconnect.enable = true;
