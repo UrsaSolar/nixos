c0e6619c6a9d39986c8bc561d9190337fd0e53bd
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-06-22 11:16:01 -0700

    Update change definitions

--- system/plasma.nix
+++ system/plasma.nix
@@ -5,16 +5,13 @@
   services.xserver.desktopManager.plasma5.enable = true;
   #services.xserver.desktopManager.plasma5.bigscreen.enable = true;
   programs.dconf.enable = true; # Fix for Firefox not seeing mouse cursor settings; https://github.com/NixOS/nixpkgs/issues/207339#issuecomment-1747101887
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.autoNumlock = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.autoNumlock = true;
   #services.xserver.displayManager.sddm.theme = "elegant-sddm";
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
   # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+  services.xserver.xkb.layout = "us";
  
   environment.systemPackages = with pkgs; [
     sddm-kcm # SDDM config module
