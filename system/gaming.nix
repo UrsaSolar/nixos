8588f2895ef08098ad4002b37fb4c5b5ac9d9742
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-06-24 18:41:41 -0700

    Add lutris

--- system/gaming.nix
+++ system/gaming.nix
@@ -1,23 +1,38 @@
 { config, pkgs, unstable, ... }:
+#let
+#  overlay-steam-unstable = final: prev: {
+#    steam = unstable.steam;
+#    protontricks = unstable.protontricks;
+#  };
+#in
 {
+
+  #nixpkgs.overlays = [ overlay-steam-unstable ]; 
 
   environment.systemPackages = with pkgs; [
     discord
     mangohud
     winetricks
+    protontricks
+    lutris
   ];
+
   programs.steam = {
     enable = true;
     extraCompatPackages = with pkgs; [ proton-ge-bin ];
-    #protontricks.enable = true;
+    #protontricks = {
+    #  enable = true;
+    #};
 #    remotePlay.openFirewall = true;
   };
+
   programs.gamemode = {
     enable = true;
     enableRenice = false;
     # https://mynixos.com/nixpkgs/option/programs.gamemode.settings
     #settings = {};
   };
+
   #programs.lutris.enable = true;
   hardware.xone.enable = true;
   hardware.steam-hardware.enable = true;
