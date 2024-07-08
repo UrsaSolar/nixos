77e8002eb8a608b943d39473d4564c27bb081e75
Solar Bear <1395386+UrsaSolar@users.noreply.github.com>
2024-07-08 15:01:10 -0700

    Remove redundant alias

--- home/solarbear-server.nix
+++ home/solarbear-server.nix
@@ -11,8 +11,8 @@
   home.username = "solarbear";
   home.homeDirectory = "/home/solarbear";
 
-  home.shellAliases = {
-    hm-rebuild = "home-manager switch --flake $NIXOSDIR#solarbear-server && source $XDG_CONFIG_HOME/zsh/.zshrc && rm $XDG_CACHE_HOME/ksycoca5*";
-  };
+  #home.shellAliases = {
+  #  hm-rebuild = "home-manager switch --flake $NIXOSDIR#solarbear-server && source $XDG_CONFIG_HOME/zsh/.zshrc && rm $XDG_CACHE_HOME/ksycoca5*";
+  #};
 
 }
