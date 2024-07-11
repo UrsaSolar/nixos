36b0708483ed7188df365bc392ae534392531910
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-07-11 08:54:08 -0700

    Add home servers to work config

--- home/terminal/ssh-work.nix
+++ home/terminal/ssh-work.nix
@@ -5,6 +5,38 @@
   programs.ssh = {
     enable = true;
     extraConfig = ''
+
+# Home
+Host jupiter
+    HostName 192.168.80.100
+    user solarbear
+Host jupiter-luks
+    Hostname 192.168.80.100
+    Port 2222
+    User root
+    RequestTTY yes
+    # RemoteCommand cryptroot-unlock
+    IdentityFile ~/.ssh/jupiter-cryptunlock
+    HostKeyAlgorithms ssh-rsa
+Host mercury
+    HostName 192.168.30.100
+    user solarbear
+Host saturn
+    HostName 192.168.80.101
+    user solarbear
+Host jupiterver
+    HostName 192.168.80.100
+    user solarbear
+    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
+Host saturnver
+    HostName 192.168.80.101
+    user solarbear
+    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
+Host mercuryver
+    HostName 192.168.30.100
+    user solarbear
+    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
+
 # Servers
 Host xwiki
     HostName 10.131.10.62
