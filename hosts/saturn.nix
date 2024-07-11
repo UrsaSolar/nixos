3aefd0d55856d0741086ccc0ac522699c2af7028
Solar Bear <1395386+UrsaSolar@users.noreply.github.com>
2024-07-10 22:17:44 -0700

    Fixing a mistake

--- hosts/saturn.nix
+++ hosts/saturn.nix
@@ -35,9 +35,6 @@
     #containers.cdi.dynamic.nvidia.enable = true; # not working?
   };
 
- # environment.systemPackages = with pkgs; [
- # ];
-
  # Borgmatic
   services.borgmatic = {
     enable = true;
@@ -69,7 +66,7 @@
     driSupport = true;
     driSupport32Bit = true;
   };
-  #services.xserver.videoDrivers = ["nvidia"]; # Load nvidia driver
+  services.xserver.videoDrivers = ["nvidia"]; # Load nvidia driver
   hardware.nvidia = {
    modesetting.enable = true; # Modesetting is required for Optimus 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
@@ -77,8 +74,8 @@
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
     powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use. (Turing or newer).
-    powerManagement.finegrained = true;
+    # REQUIRES SECOND GPU. Fine-grained power management. Turns off GPU when not in use. (Turing or newer).
+    powerManagement.finegrained = false;
     # Use the NVidia open source kernel module (not nouveau
     # supported GPUs: https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
