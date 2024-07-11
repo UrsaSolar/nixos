362f2ca5e146fe7fab1f238b86165941c7aff533
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-07-10 18:39:58 -0700

    Disable for now

--- hosts/terra.nix
+++ hosts/terra.nix
@@ -20,9 +20,9 @@
     description = "Solar Bear";
     extraGroups = [ "networkmanager" "wheel" "adbusers" ];
     shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCKhaScuhiDCTHvYRUF835VN+geeMrxFaWA0Oe+UukSOrOW3BRvVTZKirUbt2ohSTIom/ij3tKC+3TcHj7i1BJ3I5BgEYd3PitiYfhdK1LjjKGJMIe4sgYo1B7AQARSvQ8MPoPYL7RpEx553m2DRo/jGx8rj6otyRw3iNArIkpB1g2meY+/Hfn42T9Lyvsa+DTuWVyE5W8unmYJVn8qFjJ0W8p5TrlOPK1UEM4eO/I7C9Lsfb6E/s2MFQRwHD0I6K4Alyaux2P/JHygD1WKafpzh7Xo8dTWmXUmEMIDnOwndg4idZeg5vJRIuKe/IT8vu7on4PG37S/jYVbZnEBo0BK0ehRYc+PD6VWSq3EXGW2jPFNx5e4XpIEsr3KE0TxE4YethmLH1bmwP/g8Zomu5MZx3Eyp5DGy+2xGTL9mbjzaqoxNa3cVKl9hW+3mAEkFiLtiXrRTCKKnEv3OKX7Yctn2Mkb3KsxjyZTN2UbVxa/3e4v2prwz03vg/+KX2x/bws= root@mercury"
-    ];
+    #openssh.authorizedKeys.keys = [  # Enable after fixing accounts!!
+      #"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCKhaScuhiDCTHvYRUF835VN+geeMrxFaWA0Oe+UukSOrOW3BRvVTZKirUbt2ohSTIom/ij3tKC+3TcHj7i1BJ3I5BgEYd3PitiYfhdK1LjjKGJMIe4sgYo1B7AQARSvQ8MPoPYL7RpEx553m2DRo/jGx8rj6otyRw3iNArIkpB1g2meY+/Hfn42T9Lyvsa+DTuWVyE5W8unmYJVn8qFjJ0W8p5TrlOPK1UEM4eO/I7C9Lsfb6E/s2MFQRwHD0I6K4Alyaux2P/JHygD1WKafpzh7Xo8dTWmXUmEMIDnOwndg4idZeg5vJRIuKe/IT8vu7on4PG37S/jYVbZnEBo0BK0ehRYc+PD6VWSq3EXGW2jPFNx5e4XpIEsr3KE0TxE4YethmLH1bmwP/g8Zomu5MZx3Eyp5DGy+2xGTL9mbjzaqoxNa3cVKl9hW+3mAEkFiLtiXrRTCKKnEv3OKX7Yctn2Mkb3KsxjyZTN2UbVxa/3e4v2prwz03vg/+KX2x/bws= root@mercury"
+    #];
  };
 
   services.openssh.enable = true;
