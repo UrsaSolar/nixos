a839142c3452d16a66be6c6df8ea4c98df3afac3
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-07-14 16:18:32 -0700

    Add ssh unlocking

--- hosts/saturn.nix
+++ hosts/saturn.nix
@@ -20,6 +20,29 @@
     #];
   }; 
 
+  # SSH Unlocking - https://wiki.nixos.org/w/index.php?title=Remote_disk_unlocking
+  boot.initrd = {
+    availableKernelModules = [ "igc" ];
+    network = {
+      enable = true;
+      udhcpc.enable = true;
+      flushBeforeStage2 = true;
+      ssh = {
+        enable = true;
+        port = 22;
+        authorizedKeys = [ 
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUHyx/4I3LxcmzRp9d1+MLd4lt0RyctsiqyfOnBXSXl solarbear@terra"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxtCabfWGBSoEY4spPRPJLAAT6dM22ElBdnoxCiPDlU kenglish@nixos-asm"
+        ];
+        hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+      };
+      postCommands = ''
+        # Automatically ask for the password on SSH login
+        echo 'cryptsetup-askpass || echo "Unlock was successful; exiting SSH session" && exit 1' >> /root/.profile
+      '';
+    };
+  };
+
   users.users.solarbear = {
     isNormalUser = true;
     description = "Solar Bear";
