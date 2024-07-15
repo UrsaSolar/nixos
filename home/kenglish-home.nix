46828c083376ffc3ca14cbc81f931fd17f565f10
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-07-15 13:59:33 -0700

    Add hm-rebuild alias back in

--- home/kenglish-home.nix
+++ home/kenglish-home.nix
@@ -11,6 +11,7 @@
     # Shell shortcuts
     nm-asm = "nmcli c up \"Allegiant Stadium\"";
     nm-sol = "nmcli c up \"Sol\"";
+    hm-rebuild = "home-manager switch --flake $NIXOSDIR && source $XDG_CONFIG_HOME/zsh/.zshrc && rm $XDG_CACHE_HOME/ksycoca5*";
   };
 }
 
