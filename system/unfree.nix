8bb8da16b57faf5da95d9ae34c9a6f745973a6e8
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-08-10 20:20:26 -0700

    Add remaining nonfree packages

--- system/unfree.nix
+++ system/unfree.nix
@@ -3,10 +3,13 @@
 nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "unrar"
     "steam"
+    "steam-run"
+    "steam-original"
     "discord"
     "nvidia-container-toolkit"
     "nvidia-x11"
     "nvidia-persistenced"
+    "xow_dongle-firmware"
   ];
 
 }
