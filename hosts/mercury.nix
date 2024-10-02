36a805cc66a7deb37ed7b1922dca31de911a2252
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-10-02 09:20:16 -0700

    Add systemd lingering

--- hosts/mercury.nix
+++ hosts/mercury.nix
@@ -16,6 +16,7 @@
     description = "Solar Bear";
     extraGroups = [ "networkmanager" "wheel" "video" "libvirtd" ];
     shell = pkgs.zsh;
+    linger = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUHyx/4I3LxcmzRp9d1+MLd4lt0RyctsiqyfOnBXSXl solarbear@terra"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxtCabfWGBSoEY4spPRPJLAAT6dM22ElBdnoxCiPDlU kenglish@nixos-asm"
