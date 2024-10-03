9a926de1f5cdfd814464a8809252579a43b261e5
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-10-03 13:14:35 -0700

    Screwin' around

--- flake.nix
+++ flake.nix
@@ -2,57 +2,52 @@
   description = "Nix Bear";
 
   inputs = {
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
+    home-manager = { 
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
       url = "github:pjones/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
     };
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, home-manager, plasma-manager, catppuccin, nixos-wsl, ... }:
+  outputs = {
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    plasma-manager,
+    catppuccin,
+    nixos-wsl,
+    ...
+  }:
 
   let
     
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    unstable = import nixpkgs-unstable {inherit system; config = { allowUnfree = true; }; };
-
-    userSettings = {
-      locale = "en_US.UTF-8";
-      dotfilesDir = "~/.config/nixos/";
-
-      work-username = "kenglish";
-      work-name = "Kyle English";
-      work-email = "kenglish@allegiantstadium.com";
-
-      home-username = "solarbear";  
-      home-name = "Solar Bear";
-      home-email = "github@ursa.solar";
-    };
+    unstable = import nixpkgs-unstable {inherit system;};
 
   in {
 
     # System Configs
-    nixosConfigurations = 
-    {
+    nixosConfigurations = {
       nixos-wsl = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = {inherit unstable;};
         modules = [
       	  nixos-wsl.nixosModules.default {
-    	    system.stateVersion = "24.05";
-          wsl.enable = true;
-          wsl.defaultUser = "kenglish";
-	      }
+    	      system.stateVersion = "24.05";
+            wsl.enable = true;
+            wsl.defaultUser = "kenglish";
+  	      }
           catppuccin.nixosModules.catppuccin
           ./configuration-wsl.nix
           ./hosts/wsl.nix 
@@ -103,9 +98,8 @@
           ./hosts/mercury.nix
         ];
       };
-
     };
-    # Home-Manager Configs
+
     homeConfigurations = {
       kenglish = home-manager.lib.homeManagerConfiguration { 
         inherit pkgs;
