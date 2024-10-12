a3a46d84f0547a00bd6c0fbc8b11f0661e64257c
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-10-12 15:39:03 -0700

    AMD

--- hosts/terra.nix
+++ hosts/terra.nix
@@ -8,6 +8,7 @@
     ../system/base-configuration.nix
     ../system/base-packages.nix
     ../system/bootloader.nix
+    ../system/amd.nix
     ../system/ssh.nix
     ../system/graphical.nix
     ../system/plasma.nix
@@ -16,7 +17,6 @@
   ];
 
   system.stateVersion = "23.11"; # Historical reference
-  #nixpkgs.config.allowUnfree = true; #Allow unfree packages
   networking.hostName = "terra";
 
   users.users.solarbear = {
@@ -24,9 +24,6 @@
     description = "Solar Bear";
     extraGroups = [ "networkmanager" "wheel" "adbusers" ];
     shell = pkgs.zsh;
-    #openssh.authorizedKeys.keys = [  # Enable after fixing accounts!!
-      #"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCKhaScuhiDCTHvYRUF835VN+geeMrxFaWA0Oe+UukSOrOW3BRvVTZKirUbt2ohSTIom/ij3tKC+3TcHj7i1BJ3I5BgEYd3PitiYfhdK1LjjKGJMIe4sgYo1B7AQARSvQ8MPoPYL7RpEx553m2DRo/jGx8rj6otyRw3iNArIkpB1g2meY+/Hfn42T9Lyvsa+DTuWVyE5W8unmYJVn8qFjJ0W8p5TrlOPK1UEM4eO/I7C9Lsfb6E/s2MFQRwHD0I6K4Alyaux2P/JHygD1WKafpzh7Xo8dTWmXUmEMIDnOwndg4idZeg5vJRIuKe/IT8vu7on4PG37S/jYVbZnEBo0BK0ehRYc+PD6VWSq3EXGW2jPFNx5e4XpIEsr3KE0TxE4YethmLH1bmwP/g8Zomu5MZx3Eyp5DGy+2xGTL9mbjzaqoxNa3cVKl9hW+3mAEkFiLtiXrRTCKKnEv3OKX7Yctn2Mkb3KsxjyZTN2UbVxa/3e4v2prwz03vg/+KX2x/bws= root@mercury"
-    #];
  };
 
   services.openssh.enable = true;
@@ -38,9 +35,6 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
-  boot.initrd.kernelModules = [ "amdgpu" ];
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  hardware.opengl.driSupport32Bit = true;
   hardware.ckb-next.enable = true;
 
   hardware.bluetooth.enable = true;
