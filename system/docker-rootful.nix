ff6f6f2bf37a33d58e4f6274dfb70b9499722a33
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-10-14 20:14:20 -0700

    Dockerrrr

--- system/docker-rootful.nix
+++ system/docker-rootful.nix
@@ -3,6 +3,7 @@
 
   virtualisation.docker = {
     enable = true;
+    liveRestore = false; # Necessary to be disabled for swarm mode
     daemon.settings = {
       default-address-pools = [{ base = "172.16.0.0/12"; size = 24;}];
       fixed-cidr = "172.16.0.0/12";
