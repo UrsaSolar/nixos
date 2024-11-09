76a76d906004f7253ebff8382551c412145c1502
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-11-09 14:55:41 -0800

    Tmux updates

--- home/terminal/tmux.nix
+++ home/terminal/tmux.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
 
   programs.tmux = {
@@ -6,8 +6,17 @@
     mouse = true;
     #enableFzf = true; ?
     sensibleOnTop = true;
+    plugins = with pkgs; [ tmuxPlugins.cpu ];
     extraConfig = ''
+
+      set -g default-terminal "tmux-256color"
       set -ga terminal-overrides ",xterm-256color:Tc"
+      run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
+
+    '';
+    catppuccin.extraConfig = ''
+      set -g @catppuccin_window_status_style "basic"
+      set -g @catppuccin_status_modules_right "application cpu host"
     '';
   };
 
