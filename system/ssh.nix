1f67d33232a602f899780e142ff86bc54a4dba90
solarbear <solarbear@noreply.ursa.solar>
2024-11-13 12:34:07 -0800

    Update system/ssh.nix

--- system/ssh.nix
+++ system/ssh.nix
@@ -5,6 +5,7 @@ let
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUHyx/4I3LxcmzRp9d1+MLd4lt0RyctsiqyfOnBXSXl solarbear@terra"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuTwUDCM3+GaHuXkLMGYFeRqCcCHuhOblydZhMzmWrn kenglish@nixos-wsl"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINivT/RWXZhBr4CUXGlsmHzq9QQDsJ7w009gKpetqgfY kenglish@nixos-wsl"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYunoMPbBUTXFbLL0vu2Gbwh6NaEt9gJadM6rk02Lcb deck@GabeBoy"
     ];
 in
 
