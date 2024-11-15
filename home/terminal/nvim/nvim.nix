3646a6127f278382d1aabb0935094c5a081f019c
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-11-15 09:58:10 -0800

    Update nvim with more syntax highlighting features

--- home/terminal/nvim/nvim.nix
+++ home/terminal/nvim/nvim.nix
@@ -20,35 +20,37 @@
     #extraLuaConfig = builtins.readFile(./catppuccin-config.lua);
     plugins = with pkgs.vimPlugins; [
 
-      { plugin = neo-tree-nvim;
-          type = "lua";
-          config = builtins.readFile(./neotree-config.lua); }
-      { plugin = lualine-nvim;
-          type = "lua";
-          config = builtins.readFile(./lualine-config.lua); }
-#      { plugin = unstable.vimPlugins.transparent-nvim;
-#          type = "lua";
-#          config = builtins.readFile(./transparent-config.lua); }
-      { plugin = nvim-lspconfig;
-          type = "lua";
-          config = ''
-            require('lspconfig').nil_ls.setup{}
+      { plugin = neo-tree-nvim; type = "lua"; config = builtins.readFile(./neotree-config.lua); }
+      { plugin = lualine-nvim; type = "lua"; config = builtins.readFile(./lualine-config.lua); }
+      { plugin = indent-blankline-nvim; type = "lua"; config = builtins.readFile(./indent-blankline.lua); }
+      #{ plugin = unstable.vimPlugins.transparent-nvim; type = "lua"; config = builtins.readFile(./transparent-config.lua); }
+      { plugin = nvim-lspconfig; type = "lua";
+          config = ''require('lspconfig').nil_ls.setup{}
             require('lspconfig').docker_compose_language_service.setup{}
-            require('lspconfig').yamlls.setup{}
-          '';
-      }
+            require('lspconfig').yamlls.setup{}''; }
 
-      #catppuccin-nvim # currently provided by another systemwide package!
       which-key-nvim
       vim-nix
-      #nvim-lspconfig
       barbar-nvim
       nvim-web-devicons # needed for barbar
       legendary-nvim
       telescope-nvim # recommended for legendary
       dressing-nvim
       cheatsheet-nvim
-#      unstable.vimPlugins.transparent-nvim
+     #unstable.vimPlugins.transparent-nvim
+      rainbow-delimiters-nvim
+      
+      nvim-treesitter
+      nvim-treesitter-parsers.nix
+      nvim-treesitter-parsers.lua
+      nvim-treesitter-parsers.yaml
+      nvim-treesitter-parsers.helm
+      nvim-treesitter-parsers.bash
+      nvim-treesitter-parsers.python
+      nvim-treesitter-parsers.markdown
+      nvim-treesitter-parsers.dockerfile
+      #nvim-treesitter-parsers.powershell
+
 
     ];
   
