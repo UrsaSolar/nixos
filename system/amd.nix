238cadf6322deef4c73032044f031958868d487e
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2024-11-21 18:57:47 -0800

    Deprecation

--- system/amd.nix
+++ system/amd.nix
@@ -3,6 +3,7 @@
 
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
-  hardware.opengl.driSupport32Bit = true;
+  #hardware.opengl.driSupport32Bit = true;   # Renamed in 24.11
+  hardware.graphics.enable32Bit = true;
 
 }
