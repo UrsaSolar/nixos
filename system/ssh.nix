d003c48dfd1388021801be19b59b5202282c64d3
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-02-04 09:25:55 -0800

    Actually update key

--- system/ssh.nix
+++ system/ssh.nix
@@ -5,7 +5,8 @@ let
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEo3x748XXr/ftMkd9KWCwRXvcRj/ftBSMK/FLGx8JUn solarbear@terra"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuTwUDCM3+GaHuXkLMGYFeRqCcCHuhOblydZhMzmWrn kenglish@nixos-wsl"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINivT/RWXZhBr4CUXGlsmHzq9QQDsJ7w009gKpetqgfY kenglish@nixos-wsl"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYunoMPbBUTXFbLL0vu2Gbwh6NaEt9gJadM6rk02Lcb deck@GabeBoy"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYunoMPbBUTXFbLL0vu2Gbwh6NaEt9gJadM6rk02Lcb deck@GabeBoy"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzaRL6I6g7t1upSFY/t9PZ5H7RZTOG2o0//IO0IcfcL root@mercury"
     ];
 in
 
