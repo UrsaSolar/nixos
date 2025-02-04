50366e49f3c8e8ee1881106555c592892705bbed
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-02-04 09:27:08 -0800

    Allow unprivileged power control

--- system/base-configuration.nix
+++ system/base-configuration.nix
@@ -40,5 +40,22 @@
     SystemMaxUse=500M
   '';
 
+  security.polkit.extraConfig = ''
+    polkit.addRule(function (action, subject) {
+      if (
+        subject.isInGroup("users") &&
+        [
+          "org.freedesktop.login1.reboot",
+          "org.freedesktop.login1.reboot-multiple-sessions",
+          "org.freedesktop.login1.power-off",
+          "org.freedesktop.login1.power-off-multiple-sessions",
+          "org.freedesktop.login1.suspend",
+        ].indexOf(action.id) !== -1
+      ) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
 }
 
