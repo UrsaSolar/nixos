180df850338c36f23665cc64f7c38a3a00f12f81
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-02-06 14:36:28 -0800

    Test namespace remapping

--- system/docker-rootful.nix
+++ system/docker-rootful.nix
@@ -8,6 +8,7 @@
       default-address-pools = [{ base = "172.16.0.0/12"; size = 24;}];
       fixed-cidr = "172.16.0.0/12";
       bip = "172.16.0.1/24";
+      #userns-remap = "default";
     };
     autoPrune = {
       enable = true;
@@ -25,6 +26,16 @@
       "0.0.0.0:2376"
     ];
   };
+
+  # https://rdes.gitlab.io/posts/2016-08-29-enabling-dockers-user-namespaces-in-nixos.html
+  #users.users.dockremap = {
+  #  isSystemUser = true;
+  #  uid = 10000;
+  #  group = "dockremap";
+  #  subUidRanges = [ { startUid = 100000; count = 65536; } ];
+  #  subGidRanges = [ { startGid = 100000; count = 65536; } ];
+  #};
+  #users.groups.dockremap.gid = 10000;
 
   services.telegraf = {
     enable = true;
