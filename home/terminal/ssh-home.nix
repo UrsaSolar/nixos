676e682f1d04c2dd99f6efb5cd1937ca29233224
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-03-01 16:10:29 -0800

    Change ssh ports for servers

--- home/terminal/ssh-home.nix
+++ home/terminal/ssh-home.nix
@@ -10,17 +10,10 @@ PreferredAuthentications publickey,password,keyboard-interactive
 
 Host jupiter
     HostName 192.168.80.100
+    Port 222
     user solarbear
     RequestTTY yes
     RemoteCommand tmux new -A -s solarbear
-Host jupiter-luks
-    Hostname 192.168.80.100
-    Port 2222
-    User root
-    RequestTTY yes
-    # RemoteCommand cryptroot-unlock
-    IdentityFile ~/.ssh/jupiter-cryptunlock
-    HostKeyAlgorithms ssh-rsa
 Host mercury
     HostName 192.168.80.104
     Port 222
@@ -31,11 +24,9 @@ Host mercury-luks
     HostName 192.168.80.104
     user root
     StrictHostKeyChecking no
-Host mercury-old
-    HostName 192.168.30.100
-    user solarbear
 Host saturn
     HostName 192.168.80.101
+    Port 222
     user solarbear
     RequestTTY yes
     RemoteCommand tmux new -A -s solarbear
@@ -43,18 +34,6 @@ Host saturn-luks
     HostName 192.168.80.101
     user root
     StrictHostKeyChecking no
-Host jupiterver
-    HostName 192.168.80.100
-    user solarbear
-    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
-Host saturnver
-    HostName 192.168.80.101
-    user solarbear
-    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
-Host mercuryver
-    HostName 192.168.80.104
-    user solarbear
-    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
 
     '';
   };
--- home/terminal/ssh-work.nix
+++ home/terminal/ssh-work.nix
@@ -11,6 +11,7 @@ PreferredAuthentications publickey,password,keyboard-interactive
 # Home
 Host jupiter
     HostName 192.168.80.100
+    Port 222
     user solarbear
     RequestTTY yes
     RemoteCommand tmux new -A -s solarbear
@@ -32,11 +33,9 @@ Host mercury-luks
     HostName 192.168.80.104
     user root
     StrictHostKeyChecking no
-Host mercury-old
-    HostName 192.168.30.100
-    user solarbear
 Host saturn
     HostName 192.168.80.101
+    Port 222
     user solarbear
     RequestTTY yes
     RemoteCommand tmux new -A -s solarbear
@@ -44,24 +43,6 @@ Host saturn-luks
     HostName 192.168.80.101
     user root
     StrictHostKeyChecking no
-Host eunomia
-    HostName 192.168.81.100
-    Port 222
-    user solarbear
-    RequestTTY yes
-    RemoteCommand tmux new -A -s solarbear
-Host jupiterver
-    HostName 192.168.80.100
-    user solarbear
-    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
-Host saturnver
-    HostName 192.168.80.101
-    user solarbear
-    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
-Host mercuryver
-    HostName 192.168.80.104
-    user solarbear
-    RemoteCommand sed 's/[^0-9.]//g' <<< $(cat /etc/os-release | grep "VERSION_ID") && exit
 
 # Servers
 Host xwiki
