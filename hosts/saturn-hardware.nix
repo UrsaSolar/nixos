fff28b259e49487a2297e712d2e97266588b48a3
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-03-07 14:31:38 -0800

    Fix ssh

--- hosts/saturn-hardware.nix
+++ hosts/saturn-hardware.nix
@@ -5,7 +5,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "r8169" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
@@ -59,4 +59,5 @@
     enable = true;
     policy = [ "magic" ];
   };
+
 }
