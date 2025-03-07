5567acce6af768557e9133ce1e0fa56d28a3585c
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-03-07 14:31:06 -0800

    Fix ssh

--- system/ssh.nix
+++ system/ssh.nix
@@ -27,23 +27,20 @@ in
   users.users.solarbear.openssh.authorizedKeys.keys = authorized_keys;
 
   # SSH Unlocking - https://wiki.nixos.org/w/index.php?title=Remote_disk_unlocking
-  boot.initrd = {
-    availableKernelModules = [ "igc" ];
-    network = {
+  boot.initrd.network = {
+    enable = true;
+    udhcpc.enable = true;
+    flushBeforeStage2 = true;
+    ssh = {
       enable = true;
-      udhcpc.enable = true;
-      flushBeforeStage2 = true;
-      ssh = {
-        enable = true;
-        port = 22;
-        authorizedKeys = authorized_keys;
-        hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
-      };
-      postCommands = ''
-        # Automatically ask for the password on SSH login
-        echo 'cryptsetup-askpass || echo "Unlock was successful; exiting SSH session" && exit 1' >> /root/.profile
-      '';
+      port = 22;
+      authorizedKeys = authorized_keys;
+      hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
     };
+    postCommands = ''
+      # Automatically ask for the password on SSH login
+      echo 'cryptsetup-askpass || echo "Unlock was successful; exiting SSH session" && exit 1' >> /root/.profile
+    '';
   };
 
 
