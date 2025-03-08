a57c849629d10dbf212cc780165824db5fe5dda6
Solar Bear <solarbear@ursa.solar>
2025-03-07 21:38:43 -0800

    Remove unnecessary line

--- system/amd.nix
+++ system/amd.nix
@@ -3,7 +3,6 @@
 
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
-  #hardware.opengl.driSupport32Bit = true;   # Renamed in 24.11
   hardware.graphics.enable32Bit = true;
 
   # Graphics control
