7a7d27df71e91b5ad4e8d846f23ed71be3cfcca9
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-03-26 12:51:21 -0700

    Enable bash management for server

--- home/config/server.nix
+++ home/config/server.nix
@@ -4,6 +4,7 @@
   
   home.username = "solarbear";
   home.homeDirectory = "/home/solarbear";
+  programs.bash.enable = true;
 
   home.shellAliases = {
     hm-rebuild = "home-manager switch --flake $NIXOSDIR#solarbear-server && source $XDG_CONFIG_HOME/.bashrc";
