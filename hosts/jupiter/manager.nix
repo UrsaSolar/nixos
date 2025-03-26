b991b780a603495a4b7d6c4ac2c1b41d7764e4a8
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-03-26 12:26:26 -0700

    Add temp borg host

--- hosts/jupiter/manager.nix
+++ hosts/jupiter/manager.nix
@@ -16,6 +16,6 @@
 
   networking.firewall.enable = false;
 
-  #services.borgbackup.jobs."docker".repo = lib.mkForce "ssh://n4325hol@n4325hol.repo.borgbase.com/./repo";
+  services.borgbackup.jobs."docker".repo = lib.mkForce "ssh://n4325hol@n4325hol.repo.borgbase.com/./repo";
 
 }
