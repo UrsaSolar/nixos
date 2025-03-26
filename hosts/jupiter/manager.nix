98a01d1d3918f846bf1882a05b425bb39952c4f6
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-03-26 12:27:40 -0700

    Add bootloader

--- hosts/jupiter/manager.nix
+++ hosts/jupiter/manager.nix
@@ -17,5 +17,7 @@
   networking.firewall.enable = false;
 
   services.borgbackup.jobs."docker".repo = lib.mkForce "ssh://n4325hol@n4325hol.repo.borgbase.com/./repo";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
 }
