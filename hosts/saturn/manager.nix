ebcf8f4801566ea28a65e88944de8d0d34ddeeb8
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-03-29 14:06:40 -0700

    Start crafting saturn manager

---
+++ hosts/saturn/manager.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, ... }:
+
+{
+
+  imports = [
+    ./manager-hardware.nix
+    ../../system/config/base.nix
+    ../../system/config/docker.nix
+    ../../system/gpu/nvidia.nix
+    ../../system/pkgs/base.nix
+    ../../system/users/server.nix
+    ../../system/users/ssh.nix
+    #../../system/promtail.nix
+  ];
+
+  system.stateVersion = "24.11";
+  networking.hostName = "jupiter-manager";
+
+  networking.firewall.enable = true;
+
+  services.borgbackup.jobs."docker".repo = lib.mkForce "ssh://n4325hol@n4325hol.repo.borgbase.com/./repo";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  #virtualisation.docker.daemon.settings.ip = "192.168.80.124";
+  services.qemuGuest.enable = true;
+}
