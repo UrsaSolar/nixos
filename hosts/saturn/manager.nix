916fd069ed846c7d2bebdcf95f292e461157f750
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-04-08 10:57:09 -0700

    Add boot device

--- hosts/saturn/manager.nix
+++ hosts/saturn/manager.nix
@@ -18,8 +18,10 @@
   networking.firewall.enable = true;
 
   services.borgbackup.jobs."docker".repo = lib.mkForce "ssh://n4325hol@n4325hol.repo.borgbase.com/./repo";
-  boot.loader.efi.canTouchEfiVariables = true;
   
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.device = "/dev/disk/by-uuid/2AA4-95F3"; 
+
   virtualisation.docker.daemon.settings.ip = "192.168.80.22";
   services.qemuGuest.enable = true;
 }
