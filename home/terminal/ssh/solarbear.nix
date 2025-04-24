a864bf508ea3ac93698a6e5ebd696b40f2fffc64
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-04-24 09:06:03 -0700

    Fix ssh config

--- home/terminal/ssh/solarbear.nix
+++ home/terminal/ssh/solarbear.nix
@@ -8,34 +8,45 @@
 
 PreferredAuthentications publickey,password,keyboard-interactive
 
+# Home
 Host jupiter
-    HostName 192.168.80.100
+    Hostname 192.168.40.13
+    Port 22
+    User root
+    RequestTTY yes
+Host jupiter-manager
+    Hostname 192.168.80.23
     Port 222
-    user solarbear
+    User solarbear
     RequestTTY yes
     RemoteCommand tmux new -A -s solarbear
+Host truenas
+    Hostname 192.168.40.10
+    Port 222
+    User superbear
+    RequestTTY yes
 Host mercury
-    HostName 192.168.80.104
+    Hostname 192.168.40.11
+    Port 22
+    user root
+    RequestTTY yes
+Host mercury-manager
+    Hostname 192.168.80.21
     Port 222
-    user solarbear
+    User solarbear
     RequestTTY yes
     RemoteCommand tmux new -A -s solarbear
-Host mercury-luks
-    HostName 192.168.80.104
-    user root
-    StrictHostKeyChecking no
 Host saturn
-    HostName 192.168.80.101
+    HostName 192.168.40.12
+    Port 22
+    user root
+    RequestTTY yes
+    StrictHostKeyChecking no
+Host mercury-manager
+    Hostname 192.168.80.22
     Port 222
-    user solarbear
+    User solarbear
     RequestTTY yes
     RemoteCommand tmux new -A -s solarbear
-Host saturn-luks
-    HostName 192.168.80.101
-    user root
-    StrictHostKeyChecking no
-
-    '';
-  };
   
 }
