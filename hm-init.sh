e5cac8a030adb12b1f4c2ea8e521d37f4edd23ba
Solar Bear <solarbear@ursa.solar>
2025-05-02 13:41:34 -0700

    Add gpu hardware

--- hm-init.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
