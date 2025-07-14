e6cfb3ba7bddcc91d2cb80d6af258053599202f3
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-07-14 10:30:48 -0700

    Update borg

--- hosts/jupiter/manager.nix
+++ hosts/jupiter/manager.nix
@@ -14,8 +14,49 @@
 
   networking.hostName = "jupiter-manager";
 
-  services.borgbackup.jobs."docker".repo = lib.mkForce "ssh://n4325hol@n4325hol.repo.borgbase.com/./repo";
-
   virtualisation.docker.daemon.settings.ip = "192.168.80.23";
   #services.openssh.listenAddresses = [{ addr = "192.168.40.102"; port = 22; }];
+
+  services.borgbackup.jobs."docker" = {
+    repo = "ssh://o23tz4xe@o23tz4xe.repo.borgbase.com/./repo";
+    user = "root"; # required due to write permissions inside volumes
+    environment.BORG_RSH = "ssh -i /root/.ssh/id_ed25519";
+    preHook = ''
+      if find /root/borg/db/postgres -mindepth 1 -maxdepth 1 | read; then
+        for FILE in /root/borg/db/postgres/*; do
+          source $FILE;
+          PGPASSWORD=$DB_PASS ${pkgs.postgresql_16}/bin/pg_dump -U $DB_USER -h $DB_HOST -p $DB_PORT $DB_NAME \
+            > /root/borg/db/backup/$DB_NAME.postgres;
+        done
+      fi
+      if find /root/borg/db/sqlite -mindepth 1 -maxdepth 1 | read; then
+        for FILE in /root/borg/db/sqlite/*; do
+          source $FILE;
+          ${pkgs.sqlite}/bin/sqlite3 $DB_FILE .dump > /root/borg/db/backup/$DB_NAME.sqlite
+        done
+      fi
+    '';
+    paths = [
+      "/mnt/docker"
+      "/root/borg/db/backup"
+    ];
+    exclude = [
+      "/mnt/docker/syncthing"
+      "/mnt/docker/romm"
+    ];
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat /root/borg/pass";
+    };
+    compression = "auto,lzma";
+    startAt = "03:00:00";
+    persistentTimer = true; #trigger immediately if time was missed
+    prune.keep = {
+      daily = 7;
+      weekly = 4;
+      monthly = 3;
+    };
+  };
+
+
 }
--- system/config/docker.nix
+++ system/config/docker.nix
@@ -82,46 +82,7 @@
     options = [ "nfsvers=4.2" "x-systemd.automount" "noauto" "nofail" "x-systemd.device-timeout=5s" ];
   };
 
-  services.borgbackup.jobs."docker" = {
-    user = "root"; # required due to write permissions inside volumes
-    environment.BORG_RSH = "ssh -i /root/borg/id_ed25519";
-    preHook = ''
-      if find /root/borg/db/postgres -mindepth 1 -maxdepth 1 | read; then
-        for FILE in /root/borg/db/postgres/*; do
-          source $FILE;
-          PGPASSWORD=$DB_PASS ${pkgs.postgresql_16}/bin/pg_dump -U $DB_USER -h $DB_HOST -p $DB_PORT $DB_NAME \
-            > /root/borg/db/backup/$DB_NAME.postgres;
-        done
-      fi
-      if find /root/borg/db/sqlite -mindepth 1 -maxdepth 1 | read; then
-        for FILE in /root/borg/db/sqlite/*; do
-          source $FILE;
-          ${pkgs.sqlite}/bin/sqlite3 $DB_FILE .dump > /root/borg/db/backup/$DB_NAME.sqlite
-        done
-      fi
-    '';
-    paths = [
-      "/var/lib/docker/volumes"
-      "/root/borg/db/backup"
-    ];
-    encryption = {
-      mode = "repokey-blake2";
-      passCommand = "cat /root/borg/pass";
-    };
-    compression = "auto,lzma";
-    startAt = "03:00:00";
-    persistentTimer = true; #trigger immediately if time was missed
-    prune.keep = {
-      daily = 7;
-      weekly = 4;
-      monthly = 3;
-    };
-  };
-
   environment.systemPackages = with pkgs; [
-    postgresql_16
-    sqlite
-    cifs-utils
     nfs-utils
   ];
 
