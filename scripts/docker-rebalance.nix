8fc8ae3922cb12c4afbbea27702ce394fc908bcb
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-08-15 14:09:32 -0700

    Syntax is hard

--- scripts/docker-rebalance.nix
+++ scripts/docker-rebalance.nix
@@ -6,10 +6,10 @@ leader="$(docker node inspect "$hostname" --format '{{ .ManagerStatus.Leader }}'
 
 if [ "$leader" = "true" ]; then
   readarray -t <<<"$(sudo docker service ls -q)"
-  for (( i=0; i<${#MAPFILE[@]}; i++ ))
+  for (( i=0; i<''${#MAPFILE[@]}; i++ ))
   do
-    echo "Rebalancing ${MAPFILE[$i]}"
-    sudo docker service update --force "${MAPFILE[$i]}"
+    echo "Rebalancing ''${MAPFILE[$i]}"
+    sudo docker service update --force "''${MAPFILE[$i]}"
     sleep 10
   done
 else
