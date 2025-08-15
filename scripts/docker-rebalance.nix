6e733d57599842a342c9380b99a16ba0de5eb170
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-08-15 14:05:34 -0700

    Update

--- scripts/docker-rebalance.nix
+++ scripts/docker-rebalance.nix
@@ -1,18 +1,19 @@
 with import <nixpkgs> {};
-writeShellScriptBin "docker-rebalance.sh"  ''
+writeShellScriptBin "docker-rebalance.sh" ''
+
 hostname="$(hostname)"
 leader="$(docker node inspect "$hostname" --format '{{ .ManagerStatus.Leader }}')"
 
 if [ "$leader" = "true" ]; then
   readarray -t <<<"$(sudo docker service ls -q)"
-  a=0
-  while [ $a -lt ${#MAPFILE[@]} ]
+  for (( i=0; i<${#MAPFILE[@]}; i++ ))
   do
     echo "Rebalancing ${MAPFILE[$i]}"
-    sudo docker service update --force "${MAPFILE[$i++]}"
+    sudo docker service update --force "${MAPFILE[$i]}"
     sleep 10
   done
 else
-    echo "Not leader, quitting".
+  echo "\nNot leader, quitting\n".
 fi
+
 ''
