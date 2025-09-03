685592c01fb5d6123c98c250d8ce31be6fb6fbfe
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-09-03 14:09:02 -0700

    Add path

--- system/config/docker.nix
+++ system/config/docker.nix
@@ -76,6 +76,7 @@
       iptables --append DOCKER-USER -o ens19 --src 192.168.80.10 --jump DROP
       '';
     wantedBy = [ "multi-user.target" ];
+    path = [ "iptables" ];
   };
 
   #services.keepalived = {
