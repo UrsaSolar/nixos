1ffb9a433abca42c944de9f3f01f2eef47de0781
Solar Bear <81395386+UrsaSolar@users.noreply.github.com>
2025-09-24 09:21:36 -0700

    Update with defaults due to deprecation

--- home/terminal/ssh/solarbear.nix
+++ home/terminal/ssh/solarbear.nix
@@ -3,6 +3,18 @@
 
   programs.ssh = {
     enable = true;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "yes";
+      compression = false;
+      serverAliveInterval = 60;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
     extraConfig = ''
       PreferredAuthentications publickey,password,keyboard-interactive
 
